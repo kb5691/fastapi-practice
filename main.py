906decbcef403dd0bbdfbf9fe70cef0d868d6d79
oshiro <kaito.o.0628@gmail.com>
2024-02-16 18:19:56 +0900

    getリクエストの処理追加

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,3 +6,10 @@ app = FastAPI()
 @app.get("/")
 async def index():
   return {"message": "Hello World"}
+
+@app.get("/countries/")
+async def country(country_name: Optional[str] = None, country_number: Optional[int] = None):
+  return {
+    "country_name": country_name,
+    "country_number": country_number
+  }
